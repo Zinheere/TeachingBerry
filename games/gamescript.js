b17a78d32a4f51dd1eed8ad1c76d03c6ebeeb72a
Zinheere <91531924+Zinheere@users.noreply.github.com>
2023-11-13 19:23:29 +1100

    new scripts

---
+++ games/gamescript.js
@@ -0,0 +1,75 @@
+function updateVar() {
+    favicon.href = faviconPaths[sessionStorage.getItem("faviconNumber")]
+    title.innerHTML = titles[sessionStorage.getItem("titleNumber")]
+}
+
+function teachingBerry() {
+    sessionStorage.setItem("titleNumber", 0);
+    sessionStorage.setItem("faviconNumber", 0);
+    updateVar()
+}
+
+function essentialAssessment() {
+    sessionStorage.setItem("titleNumber", 1);
+    sessionStorage.setItem("faviconNumber", 1);
+    updateVar()
+}
+
+function patTesting() {
+    sessionStorage.setItem("titleNumber", 2);
+    sessionStorage.setItem("faviconNumber", 2);
+    updateVar()
+}
+
+function studyladder() {
+    sessionStorage.setItem("titleNumber", 3);
+    sessionStorage.setItem("faviconNumber", 3);
+    updateVar()
+}
+
+function nothing() {
+    sessionStorage.setItem("titleNumber", 4);
+    sessionStorage.setItem("faviconNumber", 4);
+    updateVar()
+}
+
+function newTab() {
+    sessionStorage.setItem("titleNumber", 5);
+    sessionStorage.setItem("faviconNumber", 5);
+    updateVar()
+}
+
+function seesaw() {
+    sessionStorage.setItem("titleNumber", 6);
+    sessionStorage.setItem("faviconNumber", 6);
+    updateVar()
+}
+
+const favicon = document.getElementById("favicon")
+const title = document.getElementById("title")
+const titles = [
+    "Teaching Berry | Maths",
+    "Essential Assessment",
+    "Test | ACER Online Assessment and Reporting",
+    "Studyladder, online english literacy & mathematics. Kids activity games, worksheets and lesson plans.",
+    "⠀",
+    "New Tab",
+    "Seesaw"
+];
+const faviconPaths = [
+    "../../assets/icons/teachingberry.png",
+    "../../assets/icons/essentialassessment.png",
+    "../../assets/icons/pattesting.png",
+    "../../assets/icons/studyladder.png",
+    "../../assets/icons/empty.png",
+    "NULL",
+    "../../assets/icons/seesaw.png"
+];
+
+teachingBerry()
+
+title.onload = teachingBerry()
+favicon.onload = teachingBerry()
+
+updateVar()
+teachingBerry()
