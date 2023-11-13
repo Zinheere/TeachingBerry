e81718c54f5bc7027d31cae0bee0417d693dd25b
Zinheere <91531924+Zinheere@users.noreply.github.com>
2023-11-13 17:19:17 +1100

    nothing

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 function updateVar() {
-    favicon.href = faviconPaths[0]
-    title.innerHTML = titles[0]
+    favicon.href = faviconPaths[localStorage.getItem(faviconNumber)]
+    title.innerHTML = titles[localStorage.getItem(titleNumber)]
 }
 
 function essentialAssessment() {
-    faviconNumber = 1
-    titleNumber = 1
+    localStorage.setItem("titleNumber", 1);
+    localStorage.setItem("faviconNumber",1);
     updateVar()
 }
 
 const favicon = document.getElementById("favicon")
 const title = document.getElementById("favicon")
-var faviconNumber = 0
-var titleNumber = 0
 const titles = [
     "Teaching Berry | Maths",
     "Essential Assessment",
