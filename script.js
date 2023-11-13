eeda2af7de845bae6fbdfd842872433213e4e28d
Zinheere <91531924+Zinheere@users.noreply.github.com>
2023-11-13 16:44:21 +1100

    MASSIVE UPDATE

---
+++ script.js
@@ -0,0 +1,20 @@
+function updateVar() {
+    const favicon = document.getElementById("favicon")
+    const title = document.getElementById("favicon")
+}
+
+function essentialAssessment() {
+    favicon.href = faviconPaths[0]
+}
+
+
+
+
+
+const faviconPaths = [
+    "assets/icons/essentialassessment",
+];
+const favicon = document.getElementById("favicon")
+const title = document.getElementById("favicon")
+
+window.addEventListener("load", updateVar);
